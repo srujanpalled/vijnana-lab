55c20f6af5bbf7326b46417db364dd2ebbb90c2a
srujanpalled <psrujan.0@gmail.com>
2026-03-29 19:22:12 +0530

    Cleanup: remove 67 temporary scripts and text files, zero app changes

--- check_cs.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('constants.ts', 'utf8');
-const idCS = c.indexOf("id: 'computer_science'") !== -1 ? c.indexOf("id: 'computer_science'") : c.indexOf("Computer Science");
-console.log(c.slice(idCS - 200, idCS + 200));
